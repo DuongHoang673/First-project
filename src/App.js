f4aea5f95a2183dafb6ed1521cddccb1be6c4c48
DuongHoang673 <hoangducduong03kjzg@gmail.com>
2024-04-15 00:29:04 +0700

    sua lan 2

--- src/App.js
+++ src/App.js
@@ -3,28 +3,37 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponents from './Components/MyComponent';
+import React from 'react';
 
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+class App extends React.Component{
 
-  return (
-    <div>
-      Hoàng Đức Dương đz vlol!
-      <MyComponents></MyComponents>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hoàng Đức Dương đẹp trai vlol
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+  render(){
+    return (
+      <div>
+        Hoàng Đức Dương đz vlol!
+        <MyComponents></MyComponents>
+      </div>
+    );
+  }
 }
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hoàng Đức Dương đẹp trai vlol
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/Components/MyComponent.js
+++ src/Components/MyComponent.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 class MyComponents extends React.Component {
+
+    state = {
+        name: 'Duong Hoang',
+        address: '35 ngo 204 Le Thanh Nghi Ha Noi',
+        age: 20
+      };
+
     render(){
         return(
-            <div> My first Component</div>
+            <div> 
+                My name is {this.state.name} va dia chi cua toi la {this.state.address}
+            </div>
         );
     }
 }
