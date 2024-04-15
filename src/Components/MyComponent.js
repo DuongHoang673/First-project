3b23a1df38012beb4b5003973acb76661cbbfd0c
DuongHoang673 <hoangducduong03kjzg@gmail.com>
2024-04-16 01:21:21 +0700

    video 17

--- src/Components/MyComponent.js
+++ src/Components/MyComponent.js
@@ -8,10 +8,21 @@ class MyComponents extends React.Component {
         age: 20
       };
 
+    handleOnClick(event){
+        // console.log('Hoang Duc Duong dep trai');
+        // console.log(event.pageX, event.pageY);
+    }
+
+    handleOnMouseOver(event){
+        console.log(event);
+    }
+
     render(){
         return(
             <div> 
                 My name is {this.state.name} va dia chi cua toi la {this.state.address}
+                <button onClick={this.handleOnClick} >Click me</button>
+                <button onMouseOver={this.handleOnMouseOver} >Hover me</button>
             </div>
         );
     }
